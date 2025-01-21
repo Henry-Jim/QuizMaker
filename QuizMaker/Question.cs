a761ece6a05e4f18ab2d0f35e73f1126489bbacb
Henry-Jim <115047075+Henry-Jim@users.noreply.github.com>
2025-01-21 18:09:09 +0000

    Add project files.

---
+++ QuizMaker/Question.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMaker
+{
+    [Serializable]
+    internal class Question
+    {
+        public string Text { get; set; }
+        public List<string> Answers { get; set; }
+        public HashSet<int> CorrectAnswerIndices { get; set; }
+
+        public Question() // Default constructor
+        {
+            Answers = new List<string>();
+            CorrectAnswerIndices = new HashSet<int>();
+        }
+
+        public Question(string text, List<string> answers, HashSet<int> correctAnswerIndices)
+        {
+            Text = text;
+            Answers = answers;
+            CorrectAnswerIndices = correctAnswerIndices;
+        }
+
+        public bool IsCorrect(List<int> userAnswers)
+        {
+            return CorrectAnswerIndices.SetEquals(userAnswers);
+        }
+    }
+}
---
+++ QuizMaker/Quiz.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace QuizMaker
+{
+    internal class Quiz
+    {
+        public static readonly Random random = new Random();
+
+        public List<Question> Questions { get; set; }
+
+        public Quiz()
+        {
+            Questions = new List<Question>();
+        }
+
+        public void AddQuestion(Question question)
+        {
+            Questions.Add(question);
+        }
+
+        public Question GetRandomQuestion()
+        {
+            int index = random.Next(Questions.Count);
+            return Questions[index];
+        }
+
+        public void RemoveQuestion(int index)
+        {
+            Questions.RemoveAt(index);
+        }
+
+        public void SaveToFile(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Quiz));
+            using (StreamWriter write = new StreamWriter(path))
+            {
+                serializer.Serialize(write, this);
+            }
+        }
+
+        public static Quiz LoadFromFile(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Quiz));
+            using (StreamReader reader = new StreamReader(path))
+            {
+                return (Quiz)serializer.Deserialize(reader);
+            }
+        }
+
+        public int GetQuestionCount()
+        {
+            return Questions.Count;
+        }
+    }
+}
